209b6d5976d7434653054813abec10db599bb617
limed <limed@sudoers.org>
2011-07-26 19:51:34 -0700

    Added motd manifest and added some notes

---
+++ modules/motd/manifests/init.pp
@@ -0,0 +1,14 @@
+class motd {
+
+    file { '/etc/motd':
+        ensure => 'symlink',
+        target => '/etc/motd.static'
+    }
+
+    file { '/etc/motd.static':
+        ensure  => 'present',
+        group   => 'root', owner   => 'root',
+        source  => "puppet:///files/etc/motd.static"
+    }
+
+}
