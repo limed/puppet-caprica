6686e4ab8f2d3d1eb448b7ab943d3c240b3d4600
limed <limed@sudoers.org>
2011-07-30 17:23:42 -0700

    Added mysql module

---
+++ modules/mysql/manifests/init.pp
@@ -0,0 +1,21 @@
+class mysql {
+
+    package { 'mysql-server':
+        ensure => installed
+    }
+
+    package { 'mysql-client':
+        ensure  => installed
+    }
+
+    service { 'mysqld':
+        ensure      => running,
+        hasstart    => true,
+        hasrestart  => true,
+        hasstatus   => true,
+        hasrestart  => true,
+        start       => "/etc/init.d/mysql start",
+        restart     => "/etc/init.d/mysql restart",
+        require     => Package["mysql-server"]
+    }
+}
