31351d112bd93957aa8a4b05cab0472274e150c3
limed <limed@sudoers.org>
2011-07-31 22:46:07 -0700

    Copy syslog.conf over

--- modules/logging/manifests/init.pp
+++ modules/logging/manifests/init.pp
@@ -4,10 +4,18 @@ class logging {
         ensure  => installed
     }
 
+    file { '/etc/syslog.conf':
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        source  => "puppet:///files/etc/syslog.conf"
+    }
+
     service { 'syslogd':
-        ensure  => running,
-        start   => "/etc/init.d/sysklogd start",
-        restart => "/etc/init.d/sysklogd restart",
+        ensure      => running,
+        start       => "/etc/init.d/sysklogd start",
+        restart     => "/etc/init.d/sysklogd restart",
+        subscribe   => File["/etc/syslog.conf"],
         require => Package["sysklogd"]
     }
 }
